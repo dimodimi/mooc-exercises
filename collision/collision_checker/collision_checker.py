8ae1742a634a5611a9e8a5b0942888d2ca8ad28e
Dimitrios Dimopoulos <dimodimi@ece.auth.gr>
2021-07-01 01:09:15 +0300

    Refactoring - still buggy

--- collision/collision_checker/collision_checker.py
+++ collision/collision_checker/collision_checker.py
@@ -39,20 +39,16 @@ def check_collision(
     Wcoll: List[PlacedPrimitive], robot_body: List[PlacedPrimitive], robot_pose: FriendlyPose
 ) -> bool:
     # This is just some code to get you started, but you don't have to follow it exactly
-    
-    theta_rot = np.deg2rad(robot_pose.theta_deg)
-    ct, st = np.cos(theta_rot), np.sin(theta_rot)
+
+    rototranslated_robot = [PlacedPrimitive(
+                                RT_pose(robot_pose, p.pose),
+                                type(p.primitive)(*p.primitive.__dict__.values()))
+                            for p in robot_body]
 
     # start by rototranslating the robot parts by the robot pose
-    rototranslated_robot: List[PlacedPrimitive] = [PlacedPrimitive(
-                    FriendlyPose(ct * p.pose.x - st * p.pose.y + robot_pose.x, 
-                                 st * p.pose.x + ct * p.pose.y + robot_pose.y,
-                                 p.pose.theta_deg + robot_pose.theta_deg),
-                    type(p.primitive)(*p.primitive.__dict__.values())) for p in robot_body]  #
 
     collided = check_collision_list(rototranslated_robot, Wcoll)
 
-    # return a random choice
     return collided
 
 
@@ -85,38 +81,121 @@ def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:
         raise TypeError(f"Arguments must be either a Circle or a Rectangle primitive - instead got {type(a.primitive)} and {type(b.primitive)}") 
 
 def collision_check_circle_circle(c1: PlacedPrimitive, c2: PlacedPrimitive) -> bool:
-    return (c1.pose.x - c2.pose.x)**2 + (c1.pose.y - c2.pose.y)**2 < (c1.primitive.radius + c2.primitive.radius)**2
+    return (c1.pose.x - c2.pose.x)**2 + (c1.pose.y - c2.pose.y)**2 <= (c1.primitive.radius + c2.primitive.radius)**2
 
 def collision_check_rect_rect(r1: PlacedPrimitive, r2: PlacedPrimitive) -> bool:
-    w2, h2 = r2.primitive.xmax - r2.primitive.xmin, r2.primitive.ymax - r2.primitive.ymin
-    theta2 = np.deg2rad(r2.pose.theta_deg)
+    #theta2 = np.deg2rad(r2.pose.theta_deg)
+    #theta1 = np.deg2rad(r1.pose.theta_deg)
+   
+    #w2, h2 = r2.primitive.xmax - r2.primitive.xmin, r2.primitive.ymax - r2.primitive.ymin
+    #w1, h1 = r1.primitive.xmax - r1.primitive.xmin, r1.primitive.ymax - r1.primitive.ymin
 
-    theta1 = -np.deg2rad(r1.pose.theta_deg)
-    
     # Get the second rectangle's corners in the first one's frame of reference, centered at (0, 0)
-    corners =   np.array([[np.cos(theta1), -np.sin(theta1)], [np.sin(theta1), np.cos(theta1)]]) @ \
-                (np.array([[r2.pose.x - r1.pose.x], [r2.pose.y - r1.pose.y]]) + \
-                np.array([[np.cos(theta2), -np.sin(theta2)], [np.sin(theta2), np.cos(theta2)]]) @ \
-                    np.array([[w2/2, w2/2, -w2/2, -w2/2], [h2/2, -h2/2, h2/2, -h2/2]]))
+    #corners =   np.array([[np.cos(theta1), np.sin(theta1)], [-np.sin(theta1), np.cos(theta1)]]) @ \
+    #            (np.array([[r2.pose.x - r1.pose.x], [r2.pose.y - r1.pose.y]]) + \
+    #            np.array([[np.cos(theta2-theta1), -np.sin(theta2-theta1)], [np.sin(theta2-theta1), np.cos(theta2-theta1)]]) @ \
+    #                np.array([[r2.primitive.xmax, r2.primitive.xmax, r2.primitive.xmin, r2.primitive.xmin],\
+    #                          [r2.primitive.ymax, r2.primitive.ymin, r2.primitive.ymax, r2.primitive.ymin]]))
+    
+    corners = np.array([[r2.primitive.xmax, r2.primitive.xmax, r2.primitive.xmin, r2.primitive.xmin],
+                        [r2.primitive.ymax, r2.primitive.ymin, r2.primitive.ymax, r2.primitive.ymin],
+                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
+
+    corners_final = T_matrix(r2.pose, r1.pose) @ corners
+
+
 
     # Separating line theorem for convex polygons
 
     # For the rotated rectangle get the xmin, xmax, ymin, ymax of the enclosing rectangle
-    x2_min, x2_max = np.amin(corners[0]), np.amax(corners[0])
-    y2_min, y2_max = np.amin(corners[1]), np.amax(corners[1])
+    x2_min, x2_max = np.amin(corners_final[0]), np.amax(corners_final[0])
+    y2_min, y2_max = np.amin(corners_final[1]), np.amax(corners_final[1])
 
-    x_overlap = (r1.primitive.xmin < x2_max) and (r1.primitive.xmax > x2_min)
-    y_overlap = (r1.primitive.ymin < y2_max) and (r1.primitive.ymax > y2_min)
+    x_overlap = (r1.primitive.xmin <= x2_max) and (r1.primitive.xmax >= x2_min)
+    y_overlap = (r1.primitive.ymin <= y2_max) and (r1.primitive.ymax >= y2_min)
 
-    return x_overlap and y_overlap
+    if x_overlap and y_overlap:
+        return True
+
+    # Same procedure with the origin at the second rectangle
+    #corners =   np.array([[np.cos(theta2), np.sin(theta2)], [-np.sin(theta2), np.cos(theta2)]]) @ \
+    #                (np.array([[r1.pose.x - r2.pose.x], [r1.pose.y - r2.pose.y]]) + \
+    #                np.array([[np.cos(theta1-theta2), -np.sin(theta1-theta2)], [np.sin(theta1-theta2), np.cos(theta1-theta2)]]) @ \
+    #                    np.array([[r1.primitive.xmax, r1.primitive.xmax, r1.primitive.xmin, r1.primitive.xmin],\
+    #                              [r1.primitive.ymax, r1.primitive.ymin, r1.primitive.ymax, r1.primitive.ymin]]))
+
+    corners = np.array([[r1.primitive.xmax, r1.primitive.xmax, r1.primitive.xmin, r1.primitive.xmin],
+                        [r1.primitive.ymax, r1.primitive.ymin, r1.primitive.ymax, r1.primitive.ymin],
+                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
+
+    corners_final = T_matrix(r1.pose, r2.pose) @ corners
+
+    x1_min, x1_max = np.amin(corners_final[0]), np.amax(corners_final[0])
+    y1_min, y1_max = np.amin(corners_final[1]), np.amax(corners_final[1])
+
+    x_overlap = (r2.primitive.xmin <= x1_max) and (r2.primitive.xmax >= x1_min)
+    y_overlap = (r2.primitive.ymin <= y1_max) and (r2.primitive.ymax >= y1_min)
+
+    if x_overlap and y_overlap:
+        return True
+
+    return False
+
 
 def collision_check_rect_circle(rect: PlacedPrimitive, circ: PlacedPrimitive) -> bool:
 
     # The rectangle acts as our reference frame, so transform the circle
-    theta = -np.deg2rad(rect.pose.theta_deg)
-    new_center = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]) @ \
-                    np.array([[circ.pose.x - rect.pose.x], [circ.pose.y - rect.pose.y]])
+    #theta = -np.deg2rad(rect.pose.theta_deg)
+    #new_center = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]) @ \
+    #                np.array([[circ.pose.x - rect.pose.x], [circ.pose.y - rect.pose.y]])
+
+    W = FriendlyPose(0.0, 0.0, 0.0)
+    new_center = T_matrix(rect.pose, W) @ T_matrix(circ.pose, rect.pose) @ np.array([[0.0], [0.0], [1.0]])
 
     return (new_center[0] - np.clip(new_center[0], a_min=rect.primitive.xmin, a_max=rect.primitive.xmax))**2 + \
-            (new_center[1] - np.clip(new_center[1], a_min=rect.primitive.ymin, a_max=rect.primitive.ymax))**2 < circ.primitive.radius**2
+            (new_center[1] - np.clip(new_center[1], a_min=rect.primitive.ymin, a_max=rect.primitive.ymax))**2 <= circ.primitive.radius**2
 
+
+def RT_pose(Q: FriendlyPose, prev_pose: FriendlyPose) -> FriendlyPose:
+
+    A = FriendlyPose(prev_pose.x, prev_pose.y, prev_pose.theta_deg)
+    W = FriendlyPose(0.0, 0.0, 0.0)
+    T = T_matrix(A, W)
+
+    final_pose = T @ np.array([[Q.x], [Q.y], [1.0]])
+
+    return FriendlyPose(final_pose[0, 0], final_pose[1, 0], prev_pose.theta_deg + Q.theta_deg)
+
+
+def R_matrix(theta_deg: float, inverse: bool = False) -> np.ndarray:
+    
+    # Counterclockwise rotation by theta_deg degrees
+    theta = np.deg2rad(theta_deg)
+    ct, st = np.cos(theta), np.sin(theta)
+
+    if not inverse:
+        return np.array([[ct, -st], [st, ct]], dtype=np.float32)
+    else:
+        return np.array([[ct, st], [-st, ct]], dtype=np.float32)
+
+
+def T_matrix(A: FriendlyPose, B: FriendlyPose, inverse: bool = False) -> np.ndarray:
+
+    # Transformation matrix from frame A to B (B to A if inverse=True)
+
+    T = np.zeros((3, 3), dtype=np.float32)
+    T[2, 2] = 1.0
+
+    R = R_matrix(A.theta_deg - B.theta_deg, inverse=False)
+    t = np.array([[A.x - B.x], [A.y - B.y]])
+
+    if not inverse:
+        T[:2, :2] = R
+        T[:2, 2:3] = t
+    else:
+        T[:2, :2] = R.T
+        T[:2, 2:3]  = - (R.T @ t)
+
+    return T
+
+ 
