e54840dc8d8100ac9e707273e5bd4998e332fff0
Dimitrios Dimopoulos <dimodimi@ece.auth.gr>
2021-07-06 15:53:26 +0300

    Include equality checks

--- collision/collision_checker/collision_checker.py
+++ collision/collision_checker/collision_checker.py
@@ -79,13 +79,13 @@ def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:
         raise TypeError(f"Arguments must be either a Circle or a Rectangle primitive - instead got {type(a.primitive)} and {type(b.primitive)}") 
 
 def collision_check_circle_circle(c1: PlacedPrimitive, c2: PlacedPrimitive) -> bool:
-    return (c1.pose.x - c2.pose.x)**2 + (c1.pose.y - c2.pose.y)**2 < (c1.primitive.radius + c2.primitive.radius)**2
+    return (c1.pose.x - c2.pose.x)**2 + (c1.pose.y - c2.pose.y)**2 <= (c1.primitive.radius + c2.primitive.radius)**2
 
 def collision_check_rect_rect(r1: PlacedPrimitive, r2: PlacedPrimitive) -> bool:
 
     corners = np.array([[r2.primitive.xmax, r2.primitive.xmax, r2.primitive.xmin, r2.primitive.xmin],
                         [r2.primitive.ymax, r2.primitive.ymin, r2.primitive.ymax, r2.primitive.ymin],
-                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
+                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float64)
 
     corners_tr = T_matrix(r2.pose, r1.pose) @ corners
 
@@ -95,8 +95,8 @@ def collision_check_rect_rect(r1: PlacedPrimitive, r2: PlacedPrimitive) -> bool:
     x2_min, x2_max = np.amin(corners_tr[0]), np.amax(corners_tr[0])
     y2_min, y2_max = np.amin(corners_tr[1]), np.amax(corners_tr[1])
 
-    x_overlap = (r1.primitive.xmin < x2_max) and (r1.primitive.xmax > x2_min)
-    y_overlap = (r1.primitive.ymin < y2_max) and (r1.primitive.ymax > y2_min)
+    x_overlap = (r1.primitive.xmin <= x2_max) and (r1.primitive.xmax >= x2_min)
+    y_overlap = (r1.primitive.ymin <= y2_max) and (r1.primitive.ymax >= y2_min)
 
     if not (x_overlap and y_overlap):
         return False
@@ -104,15 +104,15 @@ def collision_check_rect_rect(r1: PlacedPrimitive, r2: PlacedPrimitive) -> bool:
     # Apply the same for the other rectangle
     corners = np.array([[r1.primitive.xmax, r1.primitive.xmax, r1.primitive.xmin, r1.primitive.xmin],
                         [r1.primitive.ymax, r1.primitive.ymin, r1.primitive.ymax, r1.primitive.ymin],
-                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
+                        [1.0, 1.0, 1.0, 1.0]], dtype=np.float64)
 
     corners_tr = T_matrix(r1.pose, r2.pose) @ corners
 
     x1_min, x1_max = np.amin(corners_tr[0]), np.amax(corners_tr[0])
     y1_min, y1_max = np.amin(corners_tr[1]), np.amax(corners_tr[1])
 
-    x_overlap = (r2.primitive.xmin < x1_max) and (r2.primitive.xmax > x1_min)
-    y_overlap = (r2.primitive.ymin < y1_max) and (r2.primitive.ymax > y1_min)
+    x_overlap = (r2.primitive.xmin <= x1_max) and (r2.primitive.xmax >= x1_min)
+    y_overlap = (r2.primitive.ymin <= y1_max) and (r2.primitive.ymax >= y1_min)
 
     if not (x_overlap and y_overlap):
         return False
@@ -129,7 +129,7 @@ def collision_check_rect_circle(rect: PlacedPrimitive, circ: PlacedPrimitive) ->
                              a_min=np.array([[rect.primitive.xmin], [rect.primitive.ymin], [1.0]]),
                              a_max=np.array([[rect.primitive.xmax], [rect.primitive.ymax], [1.0]]))
 
-    return (t[0, 0]**2 + t[1, 0]**2) < circ.primitive.radius**2
+    return (t[0, 0]**2 + t[1, 0]**2) <= circ.primitive.radius**2
 
 def RT_pose(Q: FriendlyPose, prev_pose: FriendlyPose) -> FriendlyPose:
 
@@ -145,21 +145,21 @@ def R_matrix(theta_deg: float, inverse: bool = False) -> np.ndarray:
     ct, st = np.cos(theta), np.sin(theta)
 
     if not inverse:
-        return np.array([[ct, -st], [st, ct]], dtype=np.float32)
+        return np.array([[ct, -st], [st, ct]], dtype=np.float64)
     else:
-        return np.array([[ct, st], [-st, ct]], dtype=np.float32)
+        return np.array([[ct, st], [-st, ct]], dtype=np.float64)
 
 
 def T_matrix(A: FriendlyPose, B: FriendlyPose, inverse: bool = False) -> np.ndarray:
 
     # Transformation matrix from frame A to B (B to A if inverse=True)
 
-    T = np.zeros((3, 3), dtype=np.float32)
+    T = np.zeros((3, 3), dtype=np.float64)
     T[2, 2] = 1.0
 
     RA = R_matrix(A.theta_deg, inverse=False)
     RB = R_matrix(B.theta_deg, inverse=False)
-    t = np.array([[A.x - B.x], [A.y - B.y]], dtype=np.float32)
+    t = np.array([[A.x - B.x], [A.y - B.y]], dtype=np.float64)
 
     if not inverse:
         T[:2, :2] = RA @ RB.T
